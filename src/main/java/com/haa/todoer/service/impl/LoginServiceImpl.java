597a864783beee948020184cb4b1535415b93d89
albarelloshernan <albarelloshernan@users.noreply.github.com>
2022-07-19 22:43:12 -0300

    Credentials through properties.

--- src/main/java/com/haa/todoer/service/impl/LoginServiceImpl.java
+++ src/main/java/com/haa/todoer/service/impl/LoginServiceImpl.java
@@ -1,13 +1,29 @@
 package com.haa.todoer.service.impl;
 
+import com.haa.todoer.repository.ToDoRepository;
+import com.haa.todoer.repository.UsersRepository;
 import com.haa.todoer.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    ToDoRepository toDoRepository;
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Value("${todoer.userName}")
+    private String ToDoUserName;
+
+    @Value("${todoer.password}")
+    private String ToDoPass;
+
     @Override
     public boolean validateUser(String userId, String password) {
-        return userId.equalsIgnoreCase("CosmeFulanito") && password.equalsIgnoreCase("Entraro01");
+        return userId.equalsIgnoreCase(ToDoUserName) && password.equalsIgnoreCase(ToDoPass);
     }
 }
